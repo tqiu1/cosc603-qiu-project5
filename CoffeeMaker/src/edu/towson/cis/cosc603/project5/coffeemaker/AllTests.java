20f52b8a306bce0893b4cd7ce31687b5eb008843
tqiu1 <tqiu1@students.towson.edu>
2016-04-22 12:10:17 -0400

    Test 3 is done (updated)

---
+++ CoffeeMaker/src/edu/towson/cis/cosc603/project5/coffeemaker/AllTests.java
@@ -0,0 +1,11 @@
+package edu.towson.cis.cosc603.project5.coffeemaker;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CoffeeMakerTest.class })
+public class AllTests {
+
+}
--- CoffeeMaker/src/edu/towson/cis/cosc603/project5/coffeemaker/CoffeeMakerTest.java
+++ CoffeeMaker/src/edu/towson/cis/cosc603/project5/coffeemaker/CoffeeMakerTest.java
@@ -1,5 +1,9 @@
 package edu.towson.cis.cosc603.project5.coffeemaker;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 /**
@@ -10,6 +14,11 @@ public class CoffeeMakerTest extends TestCase {
 	private Inventory i;
 	private Recipe r1;
 
+	@Before 
+	/**
+	 * The setUp method that creates the necessary test objects.
+	 */
+	
 	public void setUp() {
 		cm = new CoffeeMaker();
 		i = cm.checkInventory();
@@ -17,58 +26,51 @@ public class CoffeeMakerTest extends TestCase {
 		r1 = new Recipe();
 		r1.setName("Coffee");
 		r1.setPrice(50);
-		r1.setAmtCoffee(6);
+		r1.setAmtCoffee(3);
 		r1.setAmtMilk(1);
 		r1.setAmtSugar(1);
 		r1.setAmtChocolate(0);
 	}
-
+	
+	@Test 
+	/**
+	 * Runs various equality tests against the CoffeeMaker class
+	 */
+	
+    /**
+     * Testing for addRecipe() method
+     */ 
 	public void testAddRecipe1() {
 		assertTrue(cm.addRecipe(r1));
+	
 	}
-
+	
+    /**
+     * Test for deleteRecipe() method
+     */ 
 	public void testDeleteRecipe1() {
+
 		cm.addRecipe(r1);
 		assertTrue(cm.deleteRecipe(r1));
-	}
-
-	public void testEditRecipe1() {
 		
+
+	}
+    /**
+     * Test for editRecipe() method
+     */ 
+	public void testEditRecipe1() {
+
 		cm.addRecipe(r1);
 		Recipe newRecipe = new Recipe();
 		newRecipe = r1;
-		newRecipe.setAmtSugar(2);
 		assertTrue(cm.editRecipe(r1, newRecipe));
 	}
 	
-	public void testAddRecipe11() {
-		
-		Recipe newRecipe = new Recipe();
-		newRecipe.setName("Coffee");
-		newRecipe.setPrice(50);
-		newRecipe.setAmtCoffee(3);
-		newRecipe.setAmtMilk(1);
-		newRecipe.setAmtSugar(1);
-		newRecipe.setAmtChocolate(0);
-			
-		assertTrue(cm.addRecipe(newRecipe));
-	}
-	
-	public void deleteRecipe11() {
-		
-		Recipe newRecipe = new Recipe();
-		newRecipe.setName("Coffee");
-		newRecipe.setPrice(50);
-		newRecipe.setAmtCoffee(3);
-		newRecipe.setAmtMilk(1);
-		newRecipe.setAmtSugar(1);
-		newRecipe.setAmtChocolate(0);
-		cm.addRecipe(newRecipe);
-		assertTrue(cm.deleteRecipe(newRecipe));
-	}
-	
-	public void editRecipe11() {
-		
+    /**
+     * Test for checkInventory method
+   */ 
+  
+    public void testCheckInventory() {
 		Recipe oldRecipe = new Recipe();
 		oldRecipe.setName("Coffee");
 		oldRecipe.setPrice(50);
@@ -77,9 +79,24 @@ public class CoffeeMakerTest extends TestCase {
 		oldRecipe.setAmtSugar(1);
 		oldRecipe.setAmtChocolate(0);
 		cm.addRecipe(oldRecipe);
-		Recipe newRecipe = new Recipe();
-		newRecipe = oldRecipe;
-		assertTrue(cm.editRecipe(oldRecipe, newRecipe));
-	}
-	
+		assertEquals(3,cm.checkInventory().getCoffee());
+    }
+    
+    /**
+     * Testing for addInventory method
+   */ 
+    public void testAddInventory(){
+    	assertTrue(cm.addInventory(3,4,2,1));
+    	
+    }
+    
+    /**
+     * Testing for makeCoffee method
+   */ 
+    public void testMakeCoffee(){
+ 	    	
+    	assertEquals(30, cm.makeCoffee(r1, 80));
+    	
+    }
+    
 }
